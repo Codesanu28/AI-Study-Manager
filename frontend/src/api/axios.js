eae8007a13fc8dc20d7a894f380446bb6a67eddc
Codesanu28 <kumkumy768@gmail.com>
2026-06-07 12:55:19 +0530

    Added production backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,21 +1,19 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL:
+    "https://ai-study-manager-2.onrender.com/api/v1",
 });
 
-api.interceptors.request.use(
-  (config) => {
-    const token =
-      localStorage.getItem("token");
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
-    }
-
-    return config;
+  if (token) {
+    config.headers.Authorization =
+      `Bearer ${token}`;
   }
-);
+
+  return config;
+});
 
 export default api;
